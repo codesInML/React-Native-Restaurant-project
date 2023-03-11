9e0bcceb50ef2b745b8390aeb3638481cc7c8c47
Olubo Ifeoluwa <ifeoluwaolubo@gmail.com>
2023-03-11 01:06:56 +0100

    implemented fetching data from yelp

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchAPI = async (searchTerm) => {
+    try {
+      const { data } = await yelp.get("/search", {
+        params: {
+          limit: 30,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      console.log(data.businesses);
+      setResults(data.businesses);
+    } catch (error) {
+      setErrorMessage("something went wrong");
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    searchAPI("pasta");
+  }, []);
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log("term was submitted")}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchAPI(term)}
       />
-      <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      {errorMessage && <Text>{errorMessage}</Text>}
+      <Text>We have found {results.length} results.</Text>
     </View>
   );
 };
